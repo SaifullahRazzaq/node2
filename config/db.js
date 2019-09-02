a0694a7aec26eb51ecd6455a8c3feba343bb7e37
SaifullahRazzaq <47914599+SaifullahRazzaq@users.noreply.github.com>
2019-09-03 01:37:38 +0500

    Delete db.js

--- config/db.js
+++
@@ -1,3 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://test:test@cluster0-7emjm.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true });
-module.exports = mongoose;
